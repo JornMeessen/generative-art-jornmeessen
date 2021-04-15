10d82c1e9b6b7fbb971109b0b28170bde19cc27a
JornMeessen <jornmeessen@hotmail.com>
2021-04-15 15:14:45 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,10 +5,15 @@ function setup() {
   background('#F9F8F4');
 }
 
+const is_in_circle = (x, y) => 
+  (pow(w / 2 - x, 2) + pow(w / 2 - y, 2) < 7e4)
+
 function draw() {
-  x = random(w)
-  y = random(w)
-  if (pow(w/2 - x, 2) + pow(w/2 - y, 2) < 7e4) {
-    point(x,y)
-  }
+  if (is_in_circle(x = random(w), y = random(w)))
+    while (is_in_circle(x, y)) {
+      n = noise(x, y)
+      x += sin(n * TAU)
+      y += cos(n * TAU)
+      point(x, y)
+    }
 }
