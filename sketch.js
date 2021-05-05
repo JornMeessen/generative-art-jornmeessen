a0f29f8617cda49ba2159068f4bee60170d7451a
JornMeessen <jornmeessen@hotmail.com>
2021-05-05 19:54:56 +0200

    update

--- sketch.js
+++ sketch.js
@@ -2,13 +2,13 @@
 
 var sketch = function(p) {
   var agents = [];
-  var agentCount = 4000;
-  var noiseScale = 300;
-  var noiseStrength = 10;
-  var overlayAlpha = 10;
-  var agentAlpha = 90;
-  var strokeWidth = 0.3;
-  var drawMode = 1;
+  var agentCount = 4000;  //4000
+  var noiseScale = 300;   //300
+  var noiseStrength = 10;   //10
+  var overlayAlpha = 4;   //10
+  var agentAlpha = 1000;    //90
+  var strokeWidth = 1.0;  //0.3
+  var drawMode = 1;   //1
 
   p.setup = function() {
     p.createCanvas(p.windowWidth, p.windowHeight);
@@ -19,12 +19,13 @@ var sketch = function(p) {
   };
 
   p.draw = function() {
-    p.fill(255, overlayAlpha);
+    p.fill(0, overlayAlpha);
     p.noStroke();
     p.rect(0, 0, p.width, p.height);
 
     // Draw agents
-    p.stroke(0, agentAlpha);
+    p.stroke(255, agentAlpha);
+    
     for (var i = 0; i < agentCount; i++) {
       if (drawMode == 1) agents[i].update1(noiseScale, noiseStrength, strokeWidth);
       else agents[i].update2(noiseScale, noiseStrength, strokeWidth);
@@ -32,15 +33,23 @@ var sketch = function(p) {
   };
 
   p.keyReleased = function() {
-    if (p.key == 's' || p.key == 'S') p.saveCanvas(gd.timestamp(), 'png');
+    if (p.key == 's' || p.key == 'S') p.saveCanvas('gd.timestamp()', 'png');
     if (p.key == '1') drawMode = 1;
     if (p.key == '2') drawMode = 2;
-    if (p.key == ' ') {
+    //if (p.key == 'w') agentCount = agentCount - 1000;
+    //if (p.key == 'f') agentCount = agentCount + 1000;
+    if (p.key == 'p') {
       var newNoiseSeed = p.floor(p.random(10000));
       p.noiseSeed(newNoiseSeed);
     }
     if (p.keyCode == p.DELETE || p.keyCode == p.BACKSPACE) p.background(255);
+
   };
+
+
 };
 
 var myp5 = new p5(sketch);
+
+
+
